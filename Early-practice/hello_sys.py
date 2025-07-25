ffc0618036a1d401c1925161aabe2b2d0c769b2c
Ali_Roshandel <aroshan1377@gmail.com>
2025-07-25 13:07:17 +0330

    update hello_sys

--- Early-practice/hello_sys.py
+++ Early-practice/hello_sys.py
@@ -1,5 +1,12 @@
 # Shows args
 import sys
-print(sys.argv, "Hello World")
 
-# Execute "python hello.py" in terminal to see the result of code.
+#1:
+# print(sys.argv, "Hello World")
+
+#2:
+name = sys.argv[1]
+print(f"Hello my name is {name}")
+
+# Execute "python hello.py" in terminal to see the result of 1.
+# Execute "python hello.py Ali" in terminal to see the result of 2.
